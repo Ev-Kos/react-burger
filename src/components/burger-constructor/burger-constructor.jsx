a66d7982a743bf76151c5220687c63515b14b5a3
Evgeniya Kostyrya <zhenya241191@gmail.com>
2022-08-07 11:24:05 +0300

    add PropTypes

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,79 +1,8 @@
 import constructorStyles from './burger-constructor.module.css';
-import { ConstructorElement, DragIcon, Button  } from '@ya.praktikum/react-developer-burger-ui-components';
-import diamond from "../../images/diamond.png";
-
-const ingredients = [
-    {
-        "_id":"60666c42cc7b410027a1a9b9",
-        "name":"Соус традиционный галактический",
-        "type":"sauce",
-        "proteins":42,
-        "fat":24,
-        "carbohydrates":42,
-        "calories":99,
-        "price":15,
-        "image":"https://code.s3.yandex.net/react/code/sauce-03.png",
-        "image_mobile":"https://code.s3.yandex.net/react/code/sauce-03-mobile.png",
-        "image_large":"https://code.s3.yandex.net/react/code/sauce-03-large.png",
-        "__v":0
-    },
-    {
-        "_id":"60666c42cc7b410027a1a9bc",
-        "name":"Плоды Фалленианского дерева",
-        "type":"main",
-        "proteins":20,
-        "fat":5,
-        "carbohydrates":55,
-        "calories":77,
-        "price":874,
-        "image":"https://code.s3.yandex.net/react/code/sp_1.png",
-        "image_mobile":"https://code.s3.yandex.net/react/code/sp_1-mobile.png",
-        "image_large":"https://code.s3.yandex.net/react/code/sp_1-large.png",
-        "__v":0
-    },
-    {
-        "_id":"60666c42cc7b410027a1a9bb",
-        "name":"Хрустящие минеральные кольца",
-        "type":"main",
-        "proteins":808,
-        "fat":689,
-        "carbohydrates":609,
-        "calories":986,
-        "price":300,
-        "image":"https://code.s3.yandex.net/react/code/mineral_rings.png",
-        "image_mobile":"https://code.s3.yandex.net/react/code/mineral_rings-mobile.png",
-        "image_large":"https://code.s3.yandex.net/react/code/mineral_rings-large.png",
-        "__v":0
-    },
-    {
-        "_id":"60666c42cc7b410027a1a9bd",
-        "name":"Кристаллы марсианских альфа-сахаридов",
-        "type":"main",
-        "proteins":234,
-        "fat":432,
-        "carbohydrates":111,
-        "calories":189,
-        "price":762,
-        "image":"https://code.s3.yandex.net/react/code/core.png",
-        "image_mobile":"https://code.s3.yandex.net/react/code/core-mobile.png",
-        "image_large":"https://code.s3.yandex.net/react/code/core-large.png",
-        "__v":0
-    },
-    {
-        "_id":"60666c42cc7b410027a1a9b5",
-        "name":"Говяжий метеорит (отбивная)",
-        "type":"main",
-        "proteins":800,
-        "fat":800,
-        "carbohydrates":300,
-        "calories":2674,
-        "price":3000,
-        "image":"https://code.s3.yandex.net/react/code/meat-04.png",
-        "image_mobile":"https://code.s3.yandex.net/react/code/meat-04-mobile.png",
-        "image_large":"https://code.s3.yandex.net/react/code/meat-04-large.png",
-        "__v":0
-    }
-]
+import data from '../../utils/data'
+import { ConstructorElement, DragIcon, Button, CurrencyIcon  } from '@ya.praktikum/react-developer-burger-ui-components';
+import PropTypes from 'prop-types';
+import { burgerConstructorPropTypes } from '../../utils/prop-types'
 
 const BurgerConstructor = (props) => {
     return (
@@ -90,7 +19,7 @@ const BurgerConstructor = (props) => {
             </li>
             <li>
                 <ul className={`${constructorStyles.elementScroll} mr-4`}>
-                {ingredients.map(elem => {
+                {data.map(elem => {
                 return(
                   <li key={elem._id} className={`${constructorStyles.element} mr-2`}>
                     <DragIcon type="primary" />
@@ -116,7 +45,7 @@ const BurgerConstructor = (props) => {
         <div className={`${constructorStyles.order} mr-15 mt-10`}>
           <div className={constructorStyles.priceContainer}>
             <span className={constructorStyles.price}>2000</span>
-            <img className={constructorStyles.diamond} src={diamond} alt="#"/>
+            <CurrencyIcon type="primary" />
         </div>
           <Button type="primary" size="large">
             Оформить заказ
@@ -125,6 +54,10 @@ const BurgerConstructor = (props) => {
 
     </section>
     )
+}
+
+BurgerConstructor.propTypes = {
+  data: PropTypes.arrayOf(burgerConstructorPropTypes).isRequired
 }
 
 export default BurgerConstructor
--- src/components/ingredient/ingredient.jsx
+++ src/components/ingredient/ingredient.jsx
@@ -1,19 +1,23 @@
 import ingredientStyles from "./ingredient.module.css";
-import diamond from "../../images/diamond.png";
-import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { burgerConstructorPropTypes } from '../../utils/prop-types';
 
 const Ingredient = (props) => {
   return (
-    <li key={props.data._id} className={ingredientStyles.element}>
+    <li className={ingredientStyles.element}>
       <img className="ml-4 mr-4" src={props.data.image} alt={props.data.name}/>
       <Counter count={1} size="default"/>
       <div className={`${ingredientStyles.price} mt-2 mb-2`}>
         <span className="text text_type_digits-default pr-2">{props.data.price}</span>
-        <img className={ingredientStyles.diamond} src={diamond} alt="Валюта"/>
+        <CurrencyIcon type="primary" />
       </div>
       <p className="text text text_type_main-default">{props.data.name}</p>
     </li>
   )
+}
+
+Ingredient.propTypes = {
+  data: burgerConstructorPropTypes.isRequired
 }
 
 export default Ingredient
