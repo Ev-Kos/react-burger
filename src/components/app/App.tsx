b3cc518c8322dea209c0c4dbf01159158fd6be88
Evgeniya Kostyrya <zhenya241191@gmail.com>
2022-08-15 14:11:09 +0300

    add api

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,20 +1,48 @@
 import appStyles from './app.module.css';
 import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
-import data from '../../utils/data';
 import BurgerConstructor from '../burger-constructor/burger-constructor';
+import {request} from '../../utils/api';
+import { useState, useEffect } from 'react';
 
 function App() {
-    return (
-      <>
-        <AppHeader/>
-        <main className={appStyles.content}>
-          <BurgerIngredients data={data}/>
-          <BurgerConstructor data={data}/>
-        </main>
-      </>
+  const [state, setState] = useState({
+    data: [],
+    isLoading: true,
+    hasError: false,
+  });
+
+  useEffect(() => {
+    const getNewData = () => {
+      setState({ ...state, hasError: false, isLoading: true });
+      request()
+        .then((obj) =>
+          setState({ ...state, data: obj.data, isLoading: false })
+        )
+        .catch((err) => {
+          setState({ ...state, hasError: true, isLoading: false });
+        });
+    }
+    getNewData();
+  }, []);
+
   
-    );
-  }
+
+  return (
+    <>
+      <AppHeader/>
+      <main className={appStyles.content}>
+        {state.isLoading && "Загрузка..."}
+        {state.hasError && "Произошла ошибка"}
+        {!state.isLoading && !state.hasError && (
+          <>
+          <BurgerIngredients data={state.data} />
+          <BurgerConstructor data={state.data} />
+          </>
+        )}  
+      </main>
+    </>
+  );
+}
   
-  export default App;
+export default App;
