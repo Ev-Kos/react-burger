f676d0dcd48b710010129b46db714459dfbaf8ea
Evgeniya Kostyrya <zhenya241191@gmail.com>
2022-09-14 13:45:39 +0300

    add actions and reducers

---
+++ src/services/actions/actions.js
@@ -0,0 +1,60 @@
+import {getIngredients, getOrderNumber} from '../../utils/api';
+
+export const GET_INGREDIENTS_REQUEST = 'GET_INGREDIENTS_REQUEST';
+export const GET_INGREDIENTS_SUCCESS = 'GET_INGREDIENTS_SUCCESS';
+export const GET_INGREDIENTS_FAILED = 'GET_INGREDIENTS_FAILED';
+
+export const ADD_INGREDIENT = 'ADD_INGREDIENT';
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+
+export const MOVE_ELEMENT = 'MOVE_ELEMENT';
+
+export const ADD_INGREDIENT_DATA = 'ADD_INGREDIENT_DATA';
+export const DELETE_INGREDIENT_DATA = 'DELETE_INGREDIENT_DATA';
+
+export const GET_ORDER_REQUEST = 'GET_ORDER_REQUEST';
+export const GET_ORDER_SUCCESS = 'GET_ORDER_SUCCESS';
+export const GET_ORDER_FAILED = 'GET_ORDER_FAILED';
+
+export const OPEN_INGREDIENT_MODAL = 'OPEN_INGREDIENT_MODAL';
+export const CLOSE_INGREDIENT_MODAL = 'CLOSE_INGREDIENT_MODAL';
+
+export const OPEN_ORDER_MODAL = 'OPEN_ORDER_MODAL';
+export const CLOSE_ORDER_MODAL = 'CLOSE_ORDER_MODAL';
+
+export function getItems() {
+    return function(dispatch) {
+        dispatch({
+            type: GET_INGREDIENTS_REQUEST
+        });
+
+        getIngredients().then(res => {
+            if (res && res.success) {
+            dispatch({
+                type: GET_INGREDIENTS_SUCCESS,
+                data: res.data
+            })
+            } else {
+                dispatch({type: GET_INGREDIENTS_FAILED})
+            }
+        })
+    }
+}
+
+export function getOrderNumberApi(data) {
+    return function(dispatch) {
+        dispatch({
+            type: GET_ORDER_REQUEST
+        });
+        getOrderNumber(data)
+        .then(res=> {
+          if (res && res.success) {
+            dispatch({
+              type: GET_ORDER_SUCCESS
+            })
+          } else {
+              dispatch({type: GET_ORDER_FAILED})
+          }
+        })
+    }
+  }
--- src/services/reducers/reducers.js
+++ src/services/reducers/reducers.js
@@ -1,4 +1,135 @@
-import { combineReducers } from "redux";
+import {combineReducers} from "redux";
+import {GET_INGREDIENTS_REQUEST,
+    GET_INGREDIENTS_SUCCESS,
+    GET_INGREDIENTS_FAILED,
+    OPEN_INGREDIENT_MODAL,
+    CLOSE_INGREDIENT_MODAL,
+    OPEN_ORDER_MODAL,
+    CLOSE_ORDER_MODAL,
+    GET_ORDER_SUCCESS,
+    ADD_INGREDIENT,
+    DELETE_INGREDIENT,
+    MOVE_ELEMENT,
+    ADD_INGREDIENT_DATA,
+    DELETE_INGREDIENT_DATA,
+    GET_ORDER_FAILED
+  } from '../actions/actions';
+
+const initialState = {
+    ingredients: [],
+    selectedIngredient: [],
+    detailsIngredient: {},
+    order: {
+      number: 0
+    },
+    ingredientsRequest: false,
+    ingredientsFailed: false,
+    ingredientModal: false,
+    orderModal: false,
+    orderRequest: false,
+    orderFailed: false
+};  
+
+export const ingredientsReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case GET_INGREDIENTS_REQUEST: {
+        return {
+          ...state,
+          ingredientsRequest: true
+        }
+      }
+      case GET_INGREDIENTS_SUCCESS: {
+        return {
+          ...state,
+          ingredientsRequest: false,
+          ingredientsFailed: false,
+          ingredients: action.ingredients
+        }
+      }
+      case GET_INGREDIENTS_FAILED: {
+        return {
+          ...state,
+          ingredientsRequest: false,
+          ingredientsFailed: true
+        }
+      }
+      case OPEN_INGREDIENT_MODAL: {
+        return {
+          ...state,
+          ingredientModal: true
+        }
+      }
+      case CLOSE_INGREDIENT_MODAL: {
+        return {
+          ...state,
+          ingredientModal: false
+        }
+      }
+      case ADD_INGREDIENT: {
+        return {
+          ...state,
+          selectedIngredient: [...state.selectedIngredient, action.item]
+        };
+      }
+      case DELETE_INGREDIENT: {
+        return {
+          ...state,
+          selectedIngredient: [...state.selectedIngredient].filter((item, index) => index !== action.index),
+        };
+      }
+      case MOVE_ELEMENT: {
+        return ({
+          ...state,
+          selectedIngredient: action.payload,
+        });
+      }
+      case ADD_INGREDIENT_DATA: {
+        return {
+          ...state,
+          detailsIngredient: action.item,
+        };
+      }
+      case DELETE_INGREDIENT_DATA: {
+        return {
+          ...state,
+          detailsIngredient: {},
+        };
+      }
+      case OPEN_ORDER_MODAL: {
+        return {
+          ...state,
+          orderModal: true
+        }
+      }
+      case CLOSE_ORDER_MODAL: {
+        return {
+          ...state,
+          orderModal: false
+        }
+      }
+      case GET_ORDER_SUCCESS: {
+        return {
+          ...state,
+          order: action.order,
+          orderRequest: false,
+          orderFailed: false,
+        }
+      }
+      case GET_ORDER_FAILED: {
+        return {
+          ...state,
+          order: {
+            number: 0,
+          },
+          orderRequest: false,
+          orderFailed: true,
+        };
+      }
+      default: {
+        return state
+      }
+    }
+  }
 
 export const rootReducer = combineReducers({
     data: ingredientsReducer
