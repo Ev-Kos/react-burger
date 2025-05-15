aa2864cd4ca6827a73a79c9c12c55e872d9a29dc
Evgeniia Kostyria <zhenya241191@gmail.com>
2025-05-15 13:26:50 +0300

    burger constructor

--- src/components/burger-contructor/burger-constructor.jsx
+++ src/components/burger-contructor/burger-constructor.jsx
@@ -1,5 +1,80 @@
+import { INGREDIENT_TYPES } from '@/utils/constants';
+import { ConstructorItem } from '../constructor-item/constructor-item';
 import styles from './burger-constructor.module.css';
+import {
+	ConstructorElement,
+	CurrencyIcon,
+	Button,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import { useMemo } from 'react';
 
-export const BurgerConstructor = () => {
-	return <section className={styles.burger_constructor}></section>;
+export const BurgerConstructor = ({ ingredients }) => {
+	const selectedElements = useMemo(
+		() =>
+			ingredients
+				.filter((item) => item.type !== INGREDIENT_TYPES.BUN)
+				.map((elem) => <ConstructorItem item={elem} key={elem._id} />),
+		[ingredients]
+	);
+
+	const bun = [...ingredients].find(
+		(item) => item.type === INGREDIENT_TYPES.BUN
+	);
+
+	const totalPrice = useMemo(() => {
+		return ingredients.length
+			? ingredients.reduce(
+					(total, current) =>
+						current.type !== INGREDIENT_TYPES.BUN
+							? total + current.price
+							: total + current.price * 2,
+					0
+				)
+			: 0;
+	}, [ingredients]);
+
+	return (
+		<section className={styles.burger_constructor}>
+			<ul className={styles.selected_elements}>
+				<li className={`${styles.selected_element} pr-4`}>
+					<ConstructorElement
+						type='top'
+						isLocked={true}
+						text={`${bun.name} (верх)`}
+						price={bun.price}
+						thumbnail={bun.image_mobile}
+						extraClass={styles.selected_element_hover}
+					/>
+				</li>
+				<li>
+					<ul className={`${styles.selected_elements_scroll} custom-scroll`}>
+						{selectedElements}
+					</ul>
+				</li>
+				<li className={`${styles.selected_element} pr-4`}>
+					<ConstructorElement
+						type='bottom'
+						isLocked={true}
+						text={`${bun.name} (низ)`}
+						price={bun.price}
+						thumbnail={bun.image_mobile}
+						extraClass={styles.selected_element_hover}
+					/>
+				</li>
+			</ul>
+			<div className={`${styles.order_container} mt-10 mr-4`}>
+				<div className={styles.price_container}>
+					<span className='text text_type_digits-medium'>{totalPrice}</span>
+					<CurrencyIcon type='primary' />
+				</div>
+				<Button
+					htmlType='submit'
+					type='primary'
+					size='large'
+					onClick={() => {}}>
+					Оформить заказ
+				</Button>
+			</div>
+		</section>
+	);
 };
---
+++ src/components/constructor-item/constructor-item.jsx
@@ -0,0 +1,21 @@
+import styles from './constructor-item.module.css';
+import {
+	DragIcon,
+	ConstructorElement,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+
+export const ConstructorItem = ({ item }) => {
+	return (
+		<li className={`${styles.item} pr-2 pl-4`}>
+			<DragIcon type='primary' />
+			<ConstructorElement
+				isLocked={false}
+				text={item.name}
+				price={item.price}
+				thumbnail={item.image_mobile}
+				handleClose={() => {}}
+				extraClass={styles.item_hover}
+			/>
+		</li>
+	);
+};
